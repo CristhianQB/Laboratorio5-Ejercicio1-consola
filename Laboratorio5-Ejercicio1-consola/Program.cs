901ae3df433b125dee8a9001c10b66f4fa167d0b
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-09-21 16:50:33 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio5-Ejercicio1-consola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio5_Ejercicio1_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número 1 : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el número 2 : ");
+            int num2 = int.Parse(Console.ReadLine());
+            int resul = 0;
+
+            for ( int i = 0;  i < num2;  i++)   
+            {
+                resul = resul + num1;
+            }
+                Console.WriteLine("El producto es " + resul);
+                Console.ReadKey();
+
+        }
+    }
+}
